3d2991d79a1753fea0baba43e137967fdd134483
Maleina Bidek <maleina@ucla.edu>
2021-03-09 09:13:20 -0500

    feat: Pusing click button increments count

--- Click Counter/ViewController.swift
+++ Click Counter/ViewController.swift
@@ -28,8 +28,14 @@ class ViewController: UIViewController {
         button.setTitle("Click", for: .normal)
         button.setTitleColor(UIColor.blue, for: .normal)
         view.addSubview(button)
+        
+        button.addTarget(self, action: #selector(ViewController.incrementCount), for: UIControl.Event.touchUpInside)
     }
 
+    @objc func incrementCount() {
+        self.count += 1
+        self.label.text = "\(self.count)"
+    }
 
 }
 
